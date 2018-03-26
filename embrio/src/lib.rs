b4a179afb24357edb924965b4cb5d9bcd395b11a
Wim Looman <wim.looman@kiwi.ki>
2018-03-26 11:30:11 +0200

    Rearrange optional dependency facades

--- embrio/src/lib.rs
+++ embrio/src/lib.rs
@@ -1,18 +1,20 @@
-extern crate embrio_executor;
-extern crate embrio_nrf51;
-
+#[cfg(feature = "executor")]
 pub mod executor {
-    pub use embrio_executor::Executor;
+    extern crate embrio_executor;
+
+    pub use self::embrio_executor::Executor;
 }
 
 #[cfg(feature = "nrf51")]
 pub mod nrf51 {
+    extern crate embrio_nrf51;
+
     pub mod timer {
-        pub use embrio_nrf51::timer::Timer;
+        pub use super::embrio_nrf51::timer::Timer;
     }
 
     pub mod gpio {
-        pub use embrio_nrf51::gpio::digital::Pin;
-        pub use embrio_nrf51::gpio::Pins;
+        pub use super::embrio_nrf51::gpio::digital::Pin;
+        pub use super::embrio_nrf51::gpio::Pins;
     }
 }
