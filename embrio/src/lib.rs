f9631813a6d007598043e63a2cf264371bfc50b6
Wim Looman <wim.looman@kiwi.ki>
2018-03-26 10:49:52 +0200

    Expose executor in facade

--- embrio/src/lib.rs
+++ embrio/src/lib.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate embrio_executor;
+
+pub mod executor {
+    pub use embrio_executor::Executor;
 }
