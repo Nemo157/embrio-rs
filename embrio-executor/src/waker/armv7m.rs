5c08f6142c6de2ba25bbb4b720fb22118d92b43d
Wim <wim@nemo157.com>
2018-11-19 10:28:59 +0100

    WIP: test implementation of new wake system
    See https://github.com/aturon/rfcs/pull/15

--- embrio-executor/src/waker/armv7m.rs
+++ embrio-executor/src/waker/armv7m.rs
@@ -1,12 +1,28 @@
 use core::{
     ptr::NonNull,
     sync::atomic::{AtomicBool, Ordering},
-    task::{LocalWaker, UnsafeWake, Waker},
+    task::{LocalWaker, Waker, RawWaker, RawWakerVTable},
 };
 
 pub struct EmbrioWaker {
     woken: AtomicBool,
 }
+
+static EMBRIO_WAKER_RAW_WAKER_VTABLE: RawWakerVTable = RawWakerVTable {
+    // "Unsafe closures"
+    clone: { unsafe fn clone(data: *const ()) -> RawWaker {
+        (data as *const EmbrioWaker as &'static EmbrioWaker).raw_waker()
+    } clone },
+    into_waker: { unsafe fn into_waker(data: *const ()) -> Option<RawWaker> {
+        Some((data as *const EmbrioWaker as &'static EmbrioWaker).raw_waker())
+    } into_waker },
+    wake: { unsafe fn wake(data: *const ()) {
+        (data as *const EmbrioWaker as &'static EmbrioWaker).wake()
+    } wake },
+    drop_fn: { unsafe fn drop_fn(data: *const ()) {
+        // No-op
+    } drop_fn },
+};
 
 impl EmbrioWaker {
     pub(crate) const fn new() -> Self {
@@ -17,9 +33,7 @@ impl EmbrioWaker {
 
     pub(crate) fn local_waker(&'static self) -> LocalWaker {
         unsafe {
-            LocalWaker::new(NonNull::new_unchecked(
-                &self as &UnsafeWake as *const _ as *mut _,
-            ))
+            LocalWaker::new_unchecked(self.raw_waker())
         }
     }
 
@@ -30,20 +44,18 @@ impl EmbrioWaker {
     pub(crate) fn sleep() {
         cortex_m::asm::wfe();
     }
-}
 
-unsafe impl UnsafeWake for &'static EmbrioWaker {
-    unsafe fn clone_raw(&self) -> Waker {
-        Waker::new(NonNull::new_unchecked(
-            self as &UnsafeWake as *const _ as *mut _,
-        ))
-    }
-
-    unsafe fn drop_raw(&self) {}
-
-    unsafe fn wake(&self) {
+    pub(crate) fn wake(&self) {
         self.woken.store(true, Ordering::Release);
         // we send an event in case this was a non-interrupt driven wake
         cortex_m::asm::sev();
+    }
+
+    // TODO: Is this unsafe in any way?
+    pub(crate) fn raw_waker(&'static self) -> RawWaker {
+        RawWaker {
+            data: self as *const Self as *const (),
+            vtable: &EMBRIO_WAKER_RAW_WAKER_VTABLE
+        }
     }
 }
