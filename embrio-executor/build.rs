e10a4c634c18b087e0b25791bec73e7ca40613e1
Wim Looman <wim@nemo157.com>
2019-12-22 12:54:49 +0100

    Merge if branches to satisfy clippy

--- embrio-executor/build.rs
+++ embrio-executor/build.rs
@@ -3,11 +3,13 @@ use std::env;
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    if target.starts_with("thumbv6m-") {
-        println!("cargo:rustc-cfg=armv6m");
-    } else if target.starts_with("thumbv7m-") {
-        println!("cargo:rustc-cfg=armv7m");
-    } else if target.starts_with("thumbv7em-") {
-        println!("cargo:rustc-cfg=armv7m");
+    match target.split('-').next() {
+        Some("thumbv6m") => {
+            println!("cargo:rustc-cfg=armv6m");
+        }
+        Some("thumbv7m") | Some("thumbv7em") => {
+            println!("cargo:rustc-cfg=armv7m");
+        }
+        _ => {}
     }
 }
