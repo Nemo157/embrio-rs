4124ae83e6f9ba95da967310421018f6b96861ea
Raphael Nestler <raphael.nestler@gmail.com>
2020-06-14 16:21:19 +0200

    Add an example for the microbit
    See https://www.microbit.org/ for more information about the microbit.

---
+++ examples/microbit/build.rs
@@ -0,0 +1,11 @@
+use std::{env, fs::File, io::Write, path::PathBuf};
+
+fn main() {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=memory.x");
+}
